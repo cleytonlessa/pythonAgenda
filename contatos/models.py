58b46d31433a481a7d4b279b63620d6603e71973
Cleyton Lessa <cleytonlessa@gmail.com>
2022-08-27 19:44:22 -0300

    criação de models

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,23 @@
+from time import timezone
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255, blank=True)
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    #data_criacao = models.DateTimeField(default=timezone.now)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from . models import Contato
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import wbgapi as wb
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'contatos/index.html')
+    contatos = Contato.objects.all()
+
+    """
+    desemprego = wb.data.DataFrame('SL.UEM.TOTL.ZS', economy=[
+                                   'BRA', 'ARG', 'MEX'], time=range(2016, 2021))
+    lista_paises = np.array([desemprego.index])
+    desemprego = desemprego.melt(var_name='Ano', value_name='Desemprego')
+    desemprego['País'] = np.resize(lista_paises, len(desemprego))
+    desemprego['Ano'] = pd.to_numeric(desemprego['Ano'].str.slice(start=2))
+
+    import plotly.express as px
+
+    fig = px.bar(
+    desemprego,
+    x='País',
+    y=['Desemprego'],
+    color='País',
+    animation_frame='Ano',
+    animation_group='País',
+    range_y=[0,14],
+    title='Desemprego America Latina 2010 - 2020',
+    template='plotly_dark'
+    )
+    fig.show()
+
+    teste = "cleyton"
+    """
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
